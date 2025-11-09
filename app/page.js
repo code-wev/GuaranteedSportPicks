a89cb45ca12fb23fe5ad50bbe81efe5525ae7d13
AnkonChy <ankonchy1@gmail.com>
2025-11-09 18:14:20 +0600

    members section added in home page

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ import GetFreeDailyPicks from "@/components/Home/GetFreeDailyPicks";
 import HomeBanner from "@/components/Home/HomeBanner";
 import LatestBettingInsights from "@/components/Home/LatestBettingInsights";
 import LiveOddsPicks from "@/components/Home/LiveOddsPicks";
+import Members from "@/components/Home/Members";
 import Navbar from "@/components/Home/Navbar";
 import Package from "@/components/Home/Package";
 import TodaysFeature from "@/components/Home/TodaysFeature";
@@ -16,6 +17,7 @@ export default function Page() {
       <TodaysFeature />
       <WhyChoose />
       <Package/>
+      <Members/>
       <LiveOddsPicks />
       <GetFreeDailyPicks/>
       <LatestBettingInsights/>
