e4ac87d849e210e5585b252cba7d18db4a116842
KhalidTheCoder <khalidahsan2407@gmail.com>
2025-11-09 14:36:39 +0600

    liveOddPicks component added

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
-import HomeBanner from "@/components/HomeBanner";
-import Navbar from "@/components/Navbar";
+import HomeBanner from "@/components/Home/HomeBanner";
+import LiveOddsPicks from "@/components/Home/LiveOddsPicks";
+import Navbar from "@/components/Home/Navbar";
 
 export default function Page() {
   return (
     <div>
      <Navbar></Navbar>
      <HomeBanner></HomeBanner>
+     <LiveOddsPicks></LiveOddsPicks>
     </div>
   );
 }
