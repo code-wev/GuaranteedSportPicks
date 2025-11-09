c4bd958dced5ee30bdba7c8b5c18b2f0fe466bce
AnkonChy <ankonchy1@gmail.com>
2025-11-09 14:11:56 +0600

    todays feature section added in home page

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import HomeBanner from "@/components/HomeBanner";
 import Navbar from "@/components/Navbar";
+import TodaysFeature from "@/components/TodaysFeature";
 
 export default function Page() {
   return (
     <div>
      <Navbar></Navbar>
      <HomeBanner></HomeBanner>
+     <TodaysFeature/>
     </div>
   );
 }
